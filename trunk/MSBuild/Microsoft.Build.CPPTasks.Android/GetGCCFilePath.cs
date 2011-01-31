5b06bced05911eba769837c0f3a25e9aee98d3ca
gavin.dj.pugh <gavin.dj.pugh@08dc7829-98b5-6054-c2f8-e26c18db78c9>
2011-01-31 07:20:12 +0000

    Initial Version

---
+++ trunk/MSBuild/Microsoft.Build.CPPTasks.Android/GetGCCFilePath.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.ComponentModel;
+using System.IO;
+
+using Microsoft.Build.Utilities;
+using Microsoft.Build.Framework;
+
+namespace Microsoft.Build.CPPTasks.Android
+{
+    public class GetGCCFilePath : Task
+    {
+        private string path = "";
+        [Description("Gets or sets the result."), Output]
+        public string Path
+        {
+            get { return path; }
+            set { path = value; }
+        }
+
+        public override bool Execute()
+        {
+            path = Utils.FixSlashes(path);
+
+            return true;
+        }
+    }
+}
---
+++ trunk/MSBuild/Microsoft.Build.CPPTasks.Android/GetGCCPreproFlags.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.ComponentModel;
+using System.IO;
+
+using Microsoft.Build.Utilities;
+using Microsoft.Build.Framework;
+
+namespace Microsoft.Build.CPPTasks.Android
+{
+    public class GetGCCPreproFlags : Task
+    {
+        private string symbols = "";
+        public string Symbols
+        {
+            get { return symbols; }
+            set { symbols = value; }
+        }
+
+        private string preproFlags = "";
+        [Description("Gets or sets the result."), Output]
+        public string PreproFlags
+        {
+            get { return preproFlags; }
+            set { preproFlags = value; }
+        }
+
+        public override bool Execute()
+        {
+            String[] ppsymbols = symbols.Split(';');
+
+            StringBuilder flags = new StringBuilder(1024);
+
+            foreach (string sym in ppsymbols)
+            {
+                string mutsym = sym.Trim();
+                if (mutsym != "")
+                {
+                    flags.Append("-D");
+                    flags.Append(mutsym);
+                    flags.Append(" ");
+                }
+            }
+
+            preproFlags = flags.ToString();
+
+            return true;
+        }
+    }
+}
