24669aaab0450011119c9f01a15c56b5d5cea566
gavin.dj.pugh@googlemail.com <gavin.dj.pugh@googlemail.com@08dc7829-98b5-6054-c2f8-e26c18db78c9>
2014-01-04 08:05:01 +0000

    v0.96 - 4th January 2014
      * Visual Studio 2013 has preliminary support now. It requires VS2012 to be installed alongside it. Unfortunately
        Microsoft have radically changed how MSBuild scripts implement new platforms. VS2012 express is probably fine.
      * Fix for running on the r9 NDKs. The minimum requirement is now NDK r9c.
      * New NDK sees 4.3.3 and 4.7 GCC toolchains removed, and 4.8 added. vs-android now reflects these changes.
      * GCC 4.8 however still seems to have issues when using the STL. It appears fine if you don't use STL.
        Suggestions welcome on how to address it, my Google-fu has failed me when looking up the link errors.
        Stick with GCC 4.6 if you need STL support.
      * Added support for missing Android API targets: 12, 13, and 15 through 19. Goes up to Android 4.4 API now.

--- MSBuild/vs-android.Build.CPPTasks.Android/2010/Properties/AssemblyInfo.cs
+++ MSBuild/vs-android.Build.CPPTasks.Android/2010/Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany("Gavin Pugh")]
 [assembly: AssemblyProduct("vs-android.Build.CppTasks.Android (vs2010)")]
-[assembly: AssemblyCopyright( "Copyright © Gavin Pugh 2013" )]
+[assembly: AssemblyCopyright( "Copyright © Gavin Pugh 2014" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
 
@@ -36,5 +36,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "0.9.5.0" )]
-[assembly: AssemblyFileVersion( "0.9.5.0" )]
+[assembly: AssemblyVersion( "0.9.6.0" )]
+[assembly: AssemblyFileVersion( "0.9.6.0" )]
